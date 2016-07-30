c418495b6d8abcd3959fa69e264414145e527991
Sriharsha <snagula@kent.edu>
2016-07-29 23:50:06 -0500

    Second commit

--- src/main/java/org/Harsha/messengerappRestful/resource/MessageResource.java
+++ src/main/java/org/Harsha/messengerappRestful/resource/MessageResource.java
@@ -1,17 +1,27 @@
 package org.Harsha.messengerappRestful.resource;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.Harsha.messengerappRestful.model.Message;
+import org.Harsha.messengerappRestful.service.messageService;
+
 @Path("/messages") //when there is a request to /messages in http url the methods inside of this will execute
 public class MessageResource {
 	
+	messageService messageService = new messageService();
+	
+	
+	
 	@GET //get method which returns the get method
-	@Produces(MediaType.TEXT_PLAIN) //indicates the response is plain text
-	public String getMessages()
+	//@Produces(MediaType.TEXT_PLAIN) //indicates the response is plain text
+	@Produces(MediaType.APPLICATION_XML) //indicates the response is xml response
+	public List<Message> getMessages()
 	{	
-		return "Hello World";
+		return messageService.getAllMessages();
 	}
 }
