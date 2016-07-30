697fd89245be57a663d6ad8541bb411f08502c85
Sriharsha <snagula@kent.edu>
2016-07-30 00:45:36 -0500

    fourth commit

---
+++ src/main/java/org/Harsha/messengerappRestful/database/Database.java
@@ -0,0 +1,22 @@
+package org.Harsha.messengerappRestful.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.Harsha.messengerappRestful.model.Message;
+import org.Harsha.messengerappRestful.model.Profile;
+
+public class Database {
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+	
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	}
+	
+	
+}
